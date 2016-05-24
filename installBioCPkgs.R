4965614b7c66b8225e29abec2a3ea4fe90ed0a25
markdunning <mark.dunning@gmail.com>
2016-05-24 13:00:08 +0100

    add new packages to list

--- installBioCPkgs.R
+++ installBioCPkgs.R
@@ -4,7 +4,7 @@ options(repos = c("CRAN" = "http://cran.ma.imperial.ac.uk"))
 biocLite(c("Biostrings", "biomaRt", "BSgenome","QDNAseq","exomeCopy",
         "rtracklayer", "ggbio", "vsn","gplots","RColorBrewer","htSeqTools","org.Hs.eg.db",
         "TxDb.Hsapiens.UCSC.hg19.knownGene","BSgenome.Hsapiens.UCSC.hg19",
-        "wakefield"))
+        "wakefield","VariantAnnotation"))
 #packages for ChIP on Day 4        
 
 
