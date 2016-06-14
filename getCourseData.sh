0c0749edecef2e4a8e39f9dc3f51499c885c96db
Mark Dunning <markdunning@users.noreply.github.com>
2016-06-14 09:37:19 +0100

    Update getCourseData.sh

--- getCourseData.sh
+++ getCourseData.sh
@@ -14,11 +14,12 @@ samtools index data/hapmap/NA19238.chr22.bam
 samtools view -h ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/other_exome_alignments/NA19240/exome_alignment/NA19240.mapped.solid.mosaik.YRI.exome.20111114.bam 22 | samtools view -bS - > data/hapmap/NA19240.chr22.bam
 samtools index data/hapmap/NA19240.chr22.bam
 
-samtools view -h ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/other_exome_alignments/NA19239/exome_alignment/NA19239.mapped.solid.mosaik.YRI.exome.20111114.bam | samtools view -bS - > sample1.bam
 
 wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/other_exome_alignments/NA06984/exome_alignment/NA06984.mapped.illumina.mosaik.CEU.exome.20111114.bam
 
 java -jar $PICARD DownsampleSam I=NA06984.mapped.illumina.mosaik.CEU.exome.20111114.bam O=random.bam P=0.1 VALIDATION_STRINGENCY=SILENT
+
+rm NA06984.mapped.illumina.mosaik.CEU.exome.20111114.bam
 
 samtools view -f 0x02 -b random.bam > paired.bam 
 
