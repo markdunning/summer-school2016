81fa20f287a4326f9fe4a85872961a44fadee739
Mark Dunning <markdunning@users.noreply.github.com>
2016-06-14 11:49:58 +0100

    Update getCourseData.sh
    Change to getting chromosome 20 low-coverage bams

--- getCourseData.sh
+++ getCourseData.sh
@@ -5,21 +5,23 @@ mkdir -p ref_data
 wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/human_g1k_v37.fasta.gz -P ref_data/
 gunzip ref_data/human_g1k_v37.fasta.gz
 
-samtools view -h ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/other_exome_alignments/NA19239/exome_alignment/NA19239.mapped.solid.mosaik.YRI.exome.20111114.bam 22 | samtools view -bS - > data/hapmap/NA19239.chr22.bam
-samtools index data/hapmap/NA19239.chr22.bam
+wget ftp://ftp.ncbi.nlm.nih.gov/1000genomes/ftp/phase3/data/NA12878/alignment/NA12878.chrom20.ILLUMINA.bwa.CEU.low_coverage.20121211.bam -O data/hapmap/NA12878.chr20.bam
+samtools index data/hapmap/NA12878.chr20.bam
 
-samtools view -h ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/other_exome_alignments/NA19238/exome_alignment/NA19238.mapped.illumina.mosaik.YRI.exome.20111114.bam 22 | samtools view -bS - > data/hapmap/NA19238.chr22.bam
-samtools index data/hapmap/NA19238.chr22.bam
-
-samtools view -h ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/other_exome_alignments/NA19240/exome_alignment/NA19240.mapped.solid.mosaik.YRI.exome.20111114.bam 22 | samtools view -bS - > data/hapmap/NA19240.chr22.bam
-samtools index data/hapmap/NA19240.chr22.bam
+wget ftp://ftp.ncbi.nlm.nih.gov/1000genomes/ftp/phase3/data/NA12874/alignment/NA12874.chrom20.ILLUMINA.bwa.CEU.low_coverage.20130415.bam -O data/hapmap/NA12874.chr20.bam
+samtools index data/hapmap/NA12874.chr20.bam
 
 
-wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/other_exome_alignments/NA06984/exome_alignment/NA06984.mapped.illumina.mosaik.CEU.exome.20111114.bam
+wget ftp://ftp.ncbi.nlm.nih.gov/1000genomes/ftp/phase3/data/NA12873/alignment/NA12873.chrom20.ILLUMINA.bwa.CEU.low_coverage.20130415.bam -O data/hapmap/NA12873.chr20.bam
+samtools index data/hapmap/NA12873.chr20.bam
 
-java -jar $PICARD DownsampleSam I=NA06984.mapped.illumina.mosaik.CEU.exome.20111114.bam O=random.bam P=0.1 VALIDATION_STRINGENCY=SILENT
 
-rm NA06984.mapped.illumina.mosaik.CEU.exome.20111114.bam
+
+wget ftp://ftp.ncbi.nlm.nih.gov/1000genomes/ftp/phase3/data/NA12878/alignment/NA12878.mapped.ILLUMINA.bwa.CEU.exome.20121211.bam
+
+java -jar $PICARD DownsampleSam I=NA12878.mapped.ILLUMINA.bwa.CEU.exome.20121211.bam O=random.bam P=0.1 VALIDATION_STRINGENCY=SILENT
+
+rm NA12878.mapped.ILLUMINA.bwa.CEU.exome.20121211.bam
 
 samtools view -f 0x02 -b random.bam > paired.bam 
 
